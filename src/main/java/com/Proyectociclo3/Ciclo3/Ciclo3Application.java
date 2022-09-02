ef8b4d6084c3edb70c0bc6a8c976ed9353a68e31
Jesus Serpa <jesusdavidserpa@hotmail.com>
2022-09-02 13:19:43 -0500

    Hola mundo

--- src/main/java/com/Proyectociclo3/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/Proyectociclo3/Ciclo3/Ciclo3Application.java
@@ -2,11 +2,20 @@ package com.Proyectociclo3.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
 
-	public static void main(String[] args) {
+	@GetMapping("/hello")
+	public String hello(){
+		return "Ciclo 3";
+	}
+
+	public static void main(String [] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
 	}
 
